cd2b858869e6604c3415790c4683d835834dc419
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-09-13 09:56:32 +0100

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,15 +1,10 @@
 def clock_angle(time)
 
-    time.split(":")[0] == "12" ? hour = 0 : hour = time.split(":")[0].to_i * 30
-    minute = time.split(":")[1].to_i * 6
-    return hour + minute
-    if hour > minute
-        reuturn hour - minute
-    elsif hour < minute 
-        return minute - hour
-    else 
-        return 0
-    end
+    time.split(":")[0].to_i == 12 ? hour = 0 : hour = time.split(":")[0].to_i
+    minute = time.split(":")[1].to_i
+
+    angle = 0.5 * (60*hour - 11*minute)
+    angle > 180 ? angle - 180 : angle 
 end
 
 binding.pry
